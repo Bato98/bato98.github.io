f20446eb6ce3d6d4a43ccf012bee8e155a7afc64
Bato98 <82997698+Bato98@users.noreply.github.com>
2021-06-29 16:10:14 +0200

    PROJEKT_GOTOWY
    GOTOWY_PROJEKT

--- projekt/main.js
+++ projekt/main.js
@@ -4,52 +4,47 @@ mymap.locate({ setView: true, maZoom: 12 });
 
 var lyrOSM = L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png");
 
-lyrOpentopo = L.tileLayer('https://tile.opentopomap.org/{z}/{x}/{y}.png'),
-
-lyrGoogleHyb = L.tileLayer('https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}'),
-
-lyrGoogleR = L.tileLayer('https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}'),
-
-mymap.addLayer(lyrOSM);
+(lyrOpentopo = L.tileLayer("https://tile.opentopomap.org/{z}/{x}/{y}.png")),
+  (lyrGoogleHyb = L.tileLayer(
+    "https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}"
+  )),
+  (lyrGoogleR = L.tileLayer(
+    "https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"
+  )),
+  mymap.addLayer(lyrOSM);
 
 var baseMaps = {
-    "Openstreetmap": lyrOSM,
-    "Google Road":lyrGoogleR,
-    "OpenTopoMap":lyrOpentopo,
-    "Google Satelita":lyrGoogleHyb,
-  };
+  Openstreetmap: lyrOSM,
+  "Google Road": lyrGoogleR,
+  OpenTopoMap: lyrOpentopo,
+  "Google Satelita": lyrGoogleHyb,
+};
 
-lyrJez = L.tileLayer.wms("http://localhost:8080/geoserver/projekt_prge/wms", 
-            {
-            layers: "	projekt_prge:Jezioro_sta_ABH080_N", 
-            format: 'image/png', 
-            transparent : 'true', 
-            version : '1.1.1'
-            });
+lyrJez = L.tileLayer.wms("http://localhost:8080/geoserver/projekt_prge/wms", {
+  layers: "	projekt_prge:Jezioro_sta_ABH080_N",
+  format: "image/png",
+  transparent: "true",
+  version: "1.1.1",
+});
 
+lyrOsno = L.tileLayer.wms("http://localhost:8080/geoserver/projekt_prge/wms", {
+  layers: "	projekt_prge:Punkt_osnow_PZB060_N",
+  format: "image/png",
+  transparent: "true",
+  version: "1.1.1",
+});
 
-lyrOsno = L.tileLayer.wms("http://localhost:8080/geoserver/projekt_prge/wms", 
-            {
-            layers: "	projekt_prge:Punkt_osnow_PZB060_N", 
-            format: 'image/png', 
-            transparent : 'true', 
-            version : '1.1.1'
-            });
+lyrRzek = L.tileLayer.wms("http://localhost:8080/geoserver/projekt_prge/wms", {
+  layers: "projekt_prge:Rzeka_strum_LBH140_N",
+  format: "image/png",
+  transparent: "true",
+  version: "1.1.1",
+});
 
-lyrRzek = L.tileLayer.wms("http://localhost:8080/geoserver/projekt_prge/wms", 
-            {
-            layers: "projekt_prge:Rzeka_strum_LBH140_N", 
-            format: 'image/png', 
-            transparent : 'true', 
-            version : '1.1.1'
-            });
-
-    var overlays = {
-                "Jeziora": lyrJez,
-                "Punkty_Osnowy": lyrOsno,
-                "Rzeki": lyrRzek
-              };
+var overlays = {
+  Jeziora: lyrJez,
+  Punkty_Osnowy: lyrOsno,
+  Rzeki: lyrRzek,
+};
 
 L.control.layers(baseMaps, overlays).addTo(mymap);
-
-
